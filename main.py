4d17ce4bfdf3d21679042da8b7a15605b505ff76
martinkarlik <mkarli18@student.aau.dk>
2020-12-12 14:07:27 +0100

    Cartoon style.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import numpy as np
 import requests
 from urllib.request import urlopen
 
+INPUT_VIDEO_PATH = "input_feed/havnegade_cut1.mp4"
+OUTPUT_VIDEO_PATH = "output_feed/cartoon_havnegade.avi"
 
-def transfer_style(target):
 
-    cv2.imwrite('cache.png', target)
+def transfer_style(source):
+
+    cv2.imwrite('cache.png', source)
 
     result = requests.post(
         "https://api.deepai.org/api/fast-style-transfer",
@@ -23,16 +26,26 @@ def transfer_style(target):
     return cv2.imdecode(image, -1)
 
 
-def apply_sketch(target):
+def apply_sketch(source):
 
-    horizontal = cv2.Sobel(target, 0, 1, 0, cv2.CV_64F)
+    horizontal = cv2.Sobel(source, 0, 1, 0, cv2.CV_64F)
+    vertical = cv2.Sobel(source, 0, 0, 1, cv2.CV_64F)
 
-    # the thresholds are like
-    # (variable,0,<x axis>,<y axis>,cv2.CV_64F)
-    vertical = cv2.Sobel(target, 0, 0, 1, cv2.CV_64F)
-
-    # DO the Bitwise operation
     return cv2.bitwise_or(horizontal, vertical)
+
+
+def apply_cartoon(source):
+
+    output = np.uint8(source)
+    output[:] = ((np.round((output / 255) * 10) / 10) * 255)
+
+    horizontal = cv2.Sobel(cv2.cvtColor(source, cv2.COLOR_BGR2GRAY), 0, 1, 0, cv2.CV_64F)
+    vertical = cv2.Sobel(cv2.cvtColor(source, cv2.COLOR_BGR2GRAY), 0, 0, 1, cv2.CV_64F)
+    edge_image = cv2.bitwise_or(horizontal, vertical)
+
+    output[edge_image > 50] = (0, 0, 0)
+
+    return output
 
 
 def apply_watercolor(target):
@@ -40,13 +53,13 @@ def apply_watercolor(target):
     return cv2.stylization(target, sigma_s=60, sigma_r=0.6)
 
 
-
 def main():
-    input_feed = cv2.VideoCapture("input_feed/havnegade_cut1.mp4")
-    output_feed = []
+    input_feed = cv2.VideoCapture(INPUT_VIDEO_PATH)
     fps = input_feed.get(cv2.CAP_PROP_FPS)
     frame_count = input_feed.get(cv2.CAP_PROP_FRAME_COUNT)
     frame_size = None
+
+    out = None
 
     index = 0.0
     while input_feed.isOpened():
@@ -54,26 +67,23 @@ def main():
         if frame is None or cv2.waitKey(int(1 / fps * 1000)) & 0xFF == ord('q'):
             break
 
-        stylized_frame = apply_watercolor(frame)
-        output_feed.append(stylized_frame)
+        stylized_frame = apply_cartoon(frame)
 
         if frame_size is None:
             frame_size = (stylized_frame.shape[1], stylized_frame.shape[0])
+            out = cv2.VideoWriter(OUTPUT_VIDEO_PATH, cv2.VideoWriter_fourcc(*'DIVX'), 24, frame_size)
+
+        out.write(stylized_frame)
 
         index += 1
         print("{} / {} {:10.2f}%".format(index, frame_count, index / frame_count * 100))
 
-
-
+    out.release()
     input_feed.release()
+
     cv2.destroyAllWindows()
 
-    print(frame_size)
-    out = cv2.VideoWriter('output_feed/watercolor_havnegade_cut1.avi', cv2.VideoWriter_fourcc(*'DIVX'), 24, frame_size)
-
-    for i in range(len(output_feed)):
-        out.write(output_feed[i])
-    out.release()
+    print("Frame size: {}".format(frame_size))
 
 
 main()
